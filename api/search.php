93454ed7a6d719d38d59ee6dffd08594d5c49942
Vadim Ermakov <vadim22.barnaul@gmail.com>
2020-08-14 23:51:33 +0300

    Working on DBConnector::FetchSelect() method

--- api/search.php
+++ api/search.php
@@ -1,3 +1,15 @@
 <?php
 	echo "<code>Hello there</code>";
+	require "setup.php";
+
+	$o = new DBConnector($CFGHostAddr, $CFGUsername, $CFGPassword, $CFGDBName);
+	$o->Connect() or die("Connection failed!");
+	$o->SetFields(array(
+		"EDT",
+		"NUMBER",
+		"ENG_TITLE",
+		"QUANTITY"
+	));
+	echo $o->FetchSelect();
+	
 ?>
--- api/setup.php
+++ api/setup.php
@@ -6,9 +6,11 @@
 	$CFGDBName   = "mtg_ru";
 
 	/* error codes for monitoring class behaviour */
-	define("CONN_SUCCESS",         0x00);
-	define("CONN_ERR_UNKNOWN",     0x02);
-	define("CONN_ERR_UNREACHABLE", 0x04);
+	define("DBC_CONN_SUCCESS",          0x00);
+	define("DBC_CONN_ERR_UNKNOWN",      0x02);
+	define("DBC_CONN_ERR_UNREACHABLE",  0x04);
+	define("DBC_ERR_NOFIELDS",          0x08);
+	define("DBC_QUER_ERR_NOCONNECTION", 0x10);
 	/* more errors TODO */
 
 	class DBConnector {
@@ -19,7 +21,8 @@
 		private $conn_username;
 		private $conn_password;
 		private $conn_dbname;
-		public $err_code;       // last error
+		public $error_code;     // last error
+		public $rows;           // array with assoc arrays containing rows from the table
 
 		public function __construct($_host, $_usrname, $_passwd, $_db) {
 			$this->conn_host     = $_host;
@@ -27,13 +30,14 @@
 			$this->conn_password = $_passwd;
 			$this->conn_dbname   = $_db;
 			$this->handle        = null;
-			$this->err_code      = CONN_SUCCESS;
-			$this->results       = null;
+			$this->error_code    = DBC_CONN_SUCCESS;
+			$this->result        = null;
 			$this->fields        = array();
+			$this->rows          = array();
 		}
 
 		public function __destruct() {
-			@$this->handle->close();
+			if (!empty($this->handle)) @$this->handle->close();
 		}
 
 		public function Connect() { // returns true if connection was successful
@@ -46,7 +50,7 @@
 			@$this->handle->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5);
 
 			if ($this->handle->connect_error) { // error occured, checking further
-				$this->err_code = CONN_ERR_UNKNOWN; // if the more precise error will still be unknown
+				$this->error_code = DBC_CONN_ERR_UNKNOWN; // if the more precise error will still be unknown
 
 				// check if host available using cURL
 				$c = curl_init($this->conn_host);
@@ -56,16 +60,46 @@
 				curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
 				$resp = curl_exec($c);
 				curl_close($c);
-				$this->err_code = $resp ? CONN_SUCCESS : CONN_ERR_UNREACHABLE;
+				$this->error_code = $resp ? DBC_CONN_SUCCESS : DBC_CONN_ERR_UNREACHABLE;
 			}
 
-			return $this->err_code === CONN_SUCCESS ? true : false;
+			return $this->error_code === DBC_CONN_SUCCESS ? true : false;
 		}
 
-		public function SetFields($_fields) { // TODO function to set fields to select from DB
-			if (!isset($_fields)) return false;
-			$this->fields = $_fields;
+		public function SetFields($_fields = array()) {
+			if (empty($_fields)) {
+				$this->error_code = DBC_ERR_NOFIELDS; // no fields to set
+				return false;
+			}
+			$this->fields = $_fields; /* TODO: implement some array checks and sanitizing */
 			return true;
+		}
+
+		public function FetchSelect($keyword = "%", $field_index = 0, $table = "cards") { // returns number of rows fetched. -1 means failure (see $error_code)
+			if (empty($this->handle)) {
+				$this->error_code = DBC_QUER_ERR_NOCONNECTION;
+				return -1;
+			}
+			if (empty($this->fields)) {
+				$this->error_code = DBC_ERR_NOFIELDS;
+				return -1;
+			}
+
+			$Query = "SELECT ";
+			$flds = $this->fields; // to keep the original fields safe
+			
+			foreach($flds as &$f) $f = "`$table`.`$f`";
+			$Query = $Query.join(", ", $flds)." FROM `$table` WHERE `$table`.`{$this->fields[$field_index]}` LIKE '$keyword' LIMIT 10;";
+			echo "Got query:<br>\n".$Query;
+
+			$this->result = $this->handle->query($Query);
+			$this->rows = array();
+			while($row = $this->result->fetch_assoc()) {
+				$unit = array();
+				foreach($this->fields as $field) $unit[$field] = $row[$field];
+				$this->rows[] = $unit;
+			}
+
 		}
 
 	}
